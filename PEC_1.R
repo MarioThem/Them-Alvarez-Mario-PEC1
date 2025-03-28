27713f24cfef42154004e3cf1c1f79a5f49e6591
Mario Them <mthem@uoc.edu>
2025-03-28 11:49:58 +0100

    Script para construir el objeto SUmmarizedExperiment

--- PEC_1.R
+++ PEC_1.R
@@ -4,12 +4,15 @@ library(metabolomicsWorkbenchR)
 # Se descarga el mwtab del estudio seleccionado
 mwtab<-do_query(context = "study", input_item = "study_id", input_value = "ST003570", 
                 output_item = "mwtab")
-# Se descargan los datos del estudio
+# Se descargan los datos experimentales del estudio
 datos<-as.data.frame(do_query(context = "study", input_item = "study_id", input_value = "ST003570", 
                 output_item = "data"))
 # Se descargan los datos de las muestras
 datos_muestras<-as.data.frame(do_query(context = "study", input_item = "study_id", input_value = "ST003570", 
                               output_item = "factors"))
+# Se descargan los datos de los metabolitos
+datos_metabolitos<-as.data.frame(do_query(context = "study", input_item = "study_id", input_value = "ST003570", 
+                                          output_item = "metabolites"))
 # Se construye el slot assay
 assay<-datos[ ,8:13]
 for(i in seq(1,6)){
@@ -23,9 +26,12 @@ for(i in seq(1,18)){
 rownames(assay)<-rownames[2:19]
 # Se construye el slot colData
 colData <- datos_muestras[ ,2:8]
+rownames(colData)<-colData$ST003570.local_sample_id
+colData <- colData[, -1]<-colData[, -5]
 # Se construye el slot rowData
-rowData<-
-  
+rowData<-data.frame(datos_metabolitos[ ,3:5])
+rownames(rowData)<-rownames[2:19]
+colnames(rowData)<- c("Metabolito", "refmet")
 # Se construye el objeto de tipo SummarizedExperiment
 st003570<-SummarizedExperiment(
   assay = assay, 
@@ -35,3 +41,4 @@ st003570<-SummarizedExperiment(
 assay(st003570)
 rowData(st003570)
 colData(st003570)
+metadata(st003570)

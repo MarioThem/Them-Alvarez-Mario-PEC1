06b40d6f6193b9126570381fe0d9d52f0bda1991
Mario Them <mthem@uoc.edu>
2025-03-30 21:10:04 +0200

    corrección del nombre de los metabolitos

--- analisis_exploratorio.R
+++ analisis_exploratorio.R
@@ -1,10 +1,24 @@
-library(MSPrep)
 library(ggplot2)
 library(stats)
-load("ST003570.rda")
-datos <- assay(st003570)
-# Normalizamos en logaritmo de base 10 los valores de los compuestos                      
-st003570_normalizado <- log10(datos) 
+library(SummarizedExperiment)
+load("ST003674.rda")
+# Se creaN nuevos SE con los valores de cada metabolito para cada una de las condiciones experimentales
+hombres_INT<-st003674[,st003674$ST003674.Pneumonia=="INT" & 
+                        st003674$ST003674.Sex=="M "]
+hombres_LOB<-st003674[,st003674$ST003674.Pneumonia=="LOB" & 
+                        st003674$ST003674.Sex=="M "]
+mujeres_INT<-st003674[,st003674$ST003674.Pneumonia=="INT" & 
+                        st003674$ST003674.Sex=="F "]
+mujeres_LOB<-st003674[,st003674$ST003674.Pneumonia=="LOB" & 
+                        st003674$ST003674.Sex=="F "]
+# Se calcula la media para cada metabolito en cada grupo experimental
+medias_1 <- rowMeans(assay(hombres_INT), na.rm = TRUE)
+medias_2 <- rowMeans(assay(hombres_LOB), na.rm = TRUE)
+medias_3 <- rowMeans(assay(mujeres_INT), na.rm = TRUE)
+medias_4 <- rowMeans(assay(mujeres_LOB), na.rm = TRUE)
+
+# Se crea un data frame con las medias por metabolito y grupo experimental
+
 # Boxplot para ver los valores generales de los metabolitos
 boxplot(na.omit(st003570_normalizado), 
         main="Valores de los metabolitos normalizados",
--- creacion_summarized_experiment.R
+++ creacion_summarized_experiment.R
@@ -14,25 +14,20 @@ datos_muestras<-as.data.frame(do_query(context = "study", input_item = "study_id
 datos_metabolitos<-as.data.frame(do_query(context = "study", input_item = "study_id", input_value = "ST003674", 
                                           output_item = "metabolites"))
 # Se construye el slot assay
-assay<-datos[ ,8:52]
+assay<-datos[ ,c(4,8:52)]
 for(i in seq(1,45)){
   colnames<- c(colnames, mwtab$SUBJECT_SAMPLE_FACTORS[[i]][[2]])
 }
-colnames(assay)<-colnames[2:46]
-for(i in seq(1,139)){
-  rownames<-c(rownames,
-              mwtab[["NMR_METABOLITE_DATA"]][["Metabolites"]][[i]][["Metabolite"]])
-}
-rownames(assay)<-rownames[2:140]
-assay <- assay[order(rownames(assay)),]
+colnames(assay)<-colnames[1:46]
+rownames(assay)<-assay[ ,1]
+assay <- assay[ ,-1]
 # Se construye el slot colData
 colData <- datos_muestras[ ,c(2:4,8:9)]
-rownames(colData)<-colData$ST003674.local_sample_id
 colData <- colData[order(colData$ST003674.local_sample_id),]
+colData <- colData[ ,-1]
 # Se construye el slot rowData
-rowData<-data.frame(datos_metabolitos[ ,1:5])
-rownames(rowData)<-datos_metabolitos$metabolite_name
-rowData <- rowData[order(rownames(rowData)),]
+rowData<-data.frame(datos_metabolitos[ ,1:4])
+rowData$metabolite_name<-rownames(assay)
 # Se construye el objeto de tipo SummarizedExperiment
 st003674<-SummarizedExperiment(
   assay = assay, 
